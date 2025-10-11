8422038b8dbbf368700fbcce7373d4890d84a2ec
hnmin9407 <hnmin9407i@gmail.com>
2025-10-11 19:50:24 +0900

    BETA

--- src/main/main.js
+++ src/main/main.js
@@ -2,6 +2,11 @@ const { app, BrowserWindow } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const log = require('electron-log');
 
+// 로그 경로 지정 (한글 사용자명 문제 방지)
+log.transports.file.resolvePath = () => `${app.getPath('userData')}/logs/main.log`;
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
@@ -18,19 +23,19 @@ function createWindow() {
 app.whenReady().then(() => {
   createWindow();
 
-  // 로그 저장 위치
-  autoUpdater.logger = log;
-  autoUpdater.logger.transports.file.level = 'info';
-
-  // 업데이트 체크
-  autoUpdater.checkForUpdatesAndNotify();
+  // 3초 후 업데이트 체크 (GitHub 릴리즈 확인)
+  setTimeout(() => {
+    log.info('Checking for updates...');
+    autoUpdater.checkForUpdatesAndNotify();
+  }, 3000);
 });
 
+// autoUpdater 이벤트 로그
 autoUpdater.on('checking-for-update', () => {
   log.info('Checking for update...');
 });
 autoUpdater.on('update-available', (info) => {
-  log.info('Update available: ', info);
+  log.info('Update available:', info);
 });
 autoUpdater.on('update-not-available', () => {
   log.info('No update available');
